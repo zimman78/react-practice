734a139c9615ac64d074c4324b75578553709e23
Kyle Zimmerman <zimman78@gmail.com>
2018-07-23 16:23:37 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import HelloUser from './components/helloUser'
 import FriendsContainer from './components/friendsContainer'
-// import ShowList from './components/showList'
-// import AddFriend from './components/addFriend'
 
 
 class App extends Component {
